96995ce9ba97d14dd924259672c6612bf217669f
Robin Louarn <robin.louarn@icloud.com>
2019-01-18 16:36:17 +0100

    Update LoginRepository.java
    modification des import

--- src/main/java/fr/m2till/gofootapp/repository/LoginRepository.java
+++ src/main/java/fr/m2till/gofootapp/repository/LoginRepository.java
@@ -3,8 +3,6 @@ package fr.m2till.gofootapp.repository;
 import fr.m2till.gofootapp.entity.Login;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Calendar;
-
 public interface LoginRepository extends CrudRepository<Login,String> {
 
     Login save(Login Login);
