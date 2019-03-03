137b83422330c60a11b2b206a2e0e0164ad4ede9
faran <faran@192.168.1.94>
2019-03-04 00:36:36 +0100

    Dto nouveau club

---
+++ src/main/java/fr/m2till/gofootapp/dto/NouveauClubDto.java
@@ -0,0 +1,25 @@
+package fr.m2till.gofootapp.dto;
+
+public class NouveauClubDto {
+	private Integer idUtilisateur;
+	private String nom;
+	
+	public NouveauClubDto() {
+		super();
+	}
+	public Integer getIdUtilisateur() {
+		return idUtilisateur;
+	}
+	public void setIdUtilisateur(Integer idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	};
+	
+	
+
+}
